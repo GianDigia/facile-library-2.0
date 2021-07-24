802e0f50270e02904fe84da03bf3c618d54ddaae
Gianmarco Digiacomo <gianmarcodigiacomo@fattureincloud.it>
2021-07-25 01:28:11 +0200

    Edit About page

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -1,5 +1,14 @@
 import React from 'react'
+import styled from 'styled-components'
 
-const About = (): JSX.Element => <div>About</div>
+const About = (): JSX.Element => (
+  <Wrapper>
+    An amazing <i>About</i> page.
+  </Wrapper>
+)
+
+const Wrapper = styled.div`
+  text-align: center;
+`
 
 export default About
